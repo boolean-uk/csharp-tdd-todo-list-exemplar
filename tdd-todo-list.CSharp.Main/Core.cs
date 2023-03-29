7a2ab123a15297c35eb154f804405c04520fa06a
uerbzr <nigel.sibbert@gmail.com>
2023-03-29 18:14:30 +0100

    update

--- tdd-todo-list.CSharp.Main/Core.cs
+++ tdd-todo-list.CSharp.Main/Core.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,46 @@ namespace tdd_todo_list.CSharp.Main
 {
     public class TodoList
     {
+        private List<TodoListItem> _tasks;
+        public TodoList()
+        {
+            _tasks = new List<TodoListItem>();
+        }
+        public List<TodoListItem> GetAllTasks { get { return _tasks; } }
+        public List<TodoListItem> GetCompleteTasks { get { return _tasks.Where(x => x.IsCompleted == true).ToList();  } }
+        public List<TodoListItem> GetInCompleteTasks { get { return _tasks.Where(x => x.IsCompleted == false).ToList(); } }
+        public void AddTask(TodoListItem item)
+        {
+            _tasks.Add(item);
+        }
+        public void DeleteTask(int id)
+        {
+            var taskToDelete = _tasks.Single(r => r.Id == 2);
+            _tasks.Remove(taskToDelete);
+        }
+        public void SetTaskAs(int id, bool isComplete)
+        {
+            var task = _tasks.Where(i => i.Id==id).SingleOrDefault();
+            if (task != null)
+            {
+                task.IsCompleted = isComplete;
+            }
+        }
+        public bool FindTask(int id, out TodoListItem item)
+        {            
+            if (_tasks.Exists(x => x.Id==id))
+            {
+                item = _tasks.Where(i => i.Id == id).SingleOrDefault();
+                return true;
+            }
+            item = null;
+            return false;            
+        }
+    }
+    public class TodoListItem
+    {            
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public bool IsCompleted { get; set; }
     }
 }
--- tdd-todo-list.CSharp.Test/CoreTests.cs
+++ tdd-todo-list.CSharp.Test/CoreTests.cs
@@ -1,13 +1,75 @@
-﻿using tdd_todo_list.CSharp.Main;
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+using tdd_todo_list.CSharp.Main;
 
 namespace tdd_todo_list.CSharp.Test
 {
     public class CoreTests
     {
-        private TodoList _core;
+        private TodoList _todoList;
         public CoreTests()
         {
-            _core = new TodoList();
+            _todoList = new TodoList();
+
+            _todoList.AddTask(new TodoListItem() { Id = 1, Description = "Make Bed", IsCompleted = false });
+            _todoList.AddTask(new TodoListItem() { Id = 2, Description = "Clean Kitchen", IsCompleted = false });
         }
+
+        [Test]
+        public void AddItemTest()
+        {
+            _todoList.AddTask(new TodoListItem() { Id = 3, Description = "Mow the lawn", IsCompleted = false });
+
+            Assert.IsTrue(_todoList.GetAllTasks.Exists(x => x.Id == 3));
+        }
+        [Test]
+        public void ChangeIsCompleteTest()
+        {
+            _todoList.SetTaskAs(1, true);
+            var task = _todoList.GetCompleteTasks.Where(i => i.Id == 1).SingleOrDefault();
+            if (task != null)
+            {
+                Assert.IsTrue(task.IsCompleted == true);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+        [Test]
+        public void TestOnlyComplete()
+        {
+            _todoList.GetCompleteTasks.ForEach(x =>
+            {
+                Assert.IsTrue(x.IsCompleted);
+
+            });
+        }
+        [Test]
+        public void TestOnlyInComplete()
+        {
+            _todoList.GetInCompleteTasks.ForEach(x =>
+            {
+                Assert.IsFalse(x.IsCompleted);
+
+            });
+        }
+        [TestCase(3, false)]
+        [TestCase(1, true)]
+        [TestCase(2, true)]
+        [TestCase(10, false)]
+        public void DoesTaskExist(int id, bool expected)
+        {
+            Assert.IsTrue(_todoList.FindTask(id,out TodoListItem item) == expected);
+            if(expected)
+            {
+                Assert.IsNotNull(item);
+            }
+            else
+            {
+                Assert.IsNull(item);
+            }
+        }
+
     }
 }
